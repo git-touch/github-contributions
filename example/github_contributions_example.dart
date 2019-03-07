82a9c49aeb48f9e101ebad38673bfca8072de89b
Rongjian Zhang <pd4d10@gmail.com>
2019-03-07 11:39:53 +0800

    build: stagehand create

---
+++ example/github_contributions_example.dart
@@ -0,0 +1,6 @@
+import 'package:github_contributions/github_contributions.dart';
+
+main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/github_contributions.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library github_contributions;
+
+export 'src/github_contributions_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
