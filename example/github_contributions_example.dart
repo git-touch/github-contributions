532c2adc9b32b82b6b1a3aac1da22fa91cb90a4b
Rongjian Zhang <pd4d10@gmail.com>
2019-09-11 16:49:07 +0800

    feat: add from and to params

--- example/github_contributions_example.dart
+++ example/github_contributions_example.dart
@@ -3,7 +3,17 @@ import 'package:github_contributions/github_contributions.dart';
 main() async {
   var login = 'pd4d10'; // replace this with GitHub account you want
 
-  // get color and count of this year's contribution
+  // Get latest contributions
   var contributions = await getContributions(login);
+  print(contributions[0].count);
   print(contributions[0].color);
+  print(contributions[0].date);
+
+  // Specify date
+  var contributionsByDate = await getContributions(
+    login,
+    from: '2019-09-01',
+    to: '2019-09-11',
+  );
+  print(contributionsByDate.length);
 }
--- lib/github_contributions.dart
+++ lib/github_contributions.dart
@@ -1,6 +1,32 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+/// A library to get GitHub users' contributions information, for Dart and Flutter developers
 library github_contributions;
 
-export 'src/github_contributions_base.dart';
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' show parse;
+
+class ContributionsInfo {
+  int count;
+  String color;
+  String date;
+  ContributionsInfo({this.count, this.color, this.date});
+}
+
+Future<List<ContributionsInfo>> getContributions(String login,
+    {String from, String to}) async {
+  var uri = Uri.https('github.com', '/users/$login/contributions', {
+    ...(from == null ? {} : {'from': from}),
+    ...(to == null ? {} : {'to': to}),
+  });
+
+  var res = await http.get(uri);
+  var document = parse(res.body);
+  var rectNodes =
+      document.querySelector('.js-calendar-graph-svg').querySelectorAll('rect');
+  return rectNodes.map((rectNode) {
+    return ContributionsInfo(
+      color: rectNode.attributes['fill'],
+      count: int.tryParse(rectNode.attributes['data-count']),
+      date: rectNode.attributes['data-date'],
+    );
+  }).toList();
+}
